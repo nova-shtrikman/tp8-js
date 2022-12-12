26d54ed4009d3f9f95ef35eb56d5e1a1971f9b9e
Nova <nova.shtrikman@wsu.edu>
2022-12-12 22:15:37 +0000

    added patterns and basic clicks

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 /* Java script code for memory card game*/
 
+windo.onload = function (){
+  
+  cardList = document.getElementByClassName("card"); //collection of cards
+  
+  cardCount = cardList.length; //how many cards are on the table
+  
+  for (c=0; c < cardCount; c++) {
+    cardList[c].onclick = function() {
+      this.classList.toggle("clicked");
+    }
+  }
+  
+  
+}
